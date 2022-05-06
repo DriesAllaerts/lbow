8159ab3075175a21cc5638de77a21dcbfb596809
Dries Allaerts <D.J.N.Allaerts-1@tudelft.nl>
2022-05-06 16:28:09 +0200

    Cleaning up steady state model and notebook

---
+++ pyslaw/lineartheory.py
@@ -0,0 +1,55 @@
+"""
+Library for solving linear atmospheric gravity wave problems
+"""
+import numpy as np
+
+class LinearModel(object):
+    def __init__(self,x,h,U,N):
+        # assert x, h are one-dimensional
+        # assert x is even
+        assert(x.size==h.size)
+        assert(np.unique(np.diff(x)).size==1)
+        assert(all(np.isreal(h)))
+
+        self.U = U
+        self.N = N
+        self.Nx = x.size
+        self.dx = np.unique(np.diff(x))
+
+        self.k = 2.0 * np.pi * np.fft.rfftfreq(self.Nx,self.dx)
+        self.m = self.vertical_wavenumbers()
+
+        self.hc = np.fft.rfft(h)
+        
+
+    def solve(self,varname,z):
+        assert(varname in ['eta','u','w','p'])
+        if np.isscalar(z): z = np.array([z])
+
+        if varname == 'eta':
+            var = self.hc[:,np.newaxis] * np.exp(1j*self.m[:,np.newaxis]*z)
+        elif varname == 'w':
+            # From definition w = U * d(eta)/dx
+            var = 1j*self.U*self.k[:,np.newaxis] * self.hc[:,np.newaxis] * np.exp(1j*self.m[:,np.newaxis]*z)
+        elif varname == 'u':
+            # From continuity equation du/dx + dw/dz = 0
+            var = -1j*self.U*self.m[:,np.newaxis] * self.hc[:,np.newaxis] * np.exp(1j*self.m[:,np.newaxis]*z)
+        elif varname == 'p':
+            # From x-momentum equation U * du/dx = - dp/dx
+            var = 1j * self.U**2 * self.m[:,np.newaxis] * self.hc[:,np.newaxis] * np.exp(1j*self.m[:,np.newaxis]*z)
+
+        # Set defunct modes to zero
+        var[-1,:] = 0.
+        return np.squeeze(np.fft.irfft(var,axis=0))
+
+    def vertical_wavenumbers(self):
+        m = np.zeros(self.k.shape,dtype=np.complex128)
+        #Propagating waves
+        iprop = np.where((self.U*self.k)**2>self.N**2)
+        #Evanescent waves (excluding where U*k=0, for which m is set to zero) 
+        ievan = np.where(~(((-self.U*self.k)==0) | ((self.U*self.k)**2>self.N**2)))
+    
+        m[iprop] = 1j*np.abs(self.k[iprop])*np.sqrt(1-self.N**2/(self.U*self.k[iprop])**2)
+        m[ievan] = -np.sign(-self.U*self.k[ievan])*np.abs(self.k[ievan])*np.sqrt(self.N**2/(self.U*self.k[ievan])**2-1)
+        return m
+
---
+++ pyslaw/visualization.py
@@ -0,0 +1,15 @@
+"""
+Tools for visualization
+"""
+import numpy as np
+from matplotlib.colors import Normalize
+
+class midpointNormalize(Normalize):
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        # Ignoring masked values and all kinds of edge cases
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y))
