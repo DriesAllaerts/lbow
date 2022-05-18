612f3e831affd489446d2da857b45b2861f70e5a
Dries Allaerts <D.J.N.Allaerts-1@tudelft.nl>
2022-05-18 10:25:19 +0200

    Implement model for trapped wave solutions

--- pyslaw/lineartheory.py
+++ pyslaw/lineartheory.py
@@ -5,7 +5,7 @@ import numpy as np
 
 #TODO: Decide on sign of m in the solution, not when calculating m in general
 #TODO: Make iprop and ievan separate functions
-class LinearModel(object):
+class OneLayerModel(object):
     def __init__(self,x,h,U,N):
         """Initialize model and set governing parameters
 
@@ -34,8 +34,32 @@ class LinearModel(object):
 
         # Store FFT of input signal h
         self.hc = np.fft.rfft(h)
-        
 
+    def vertical_wavenumbers(self):
+        """Calculate vertical wavenumbers
+
+        Returns:
+            array: vertical wave numbers
+        """
+        m = np.zeros(self.k.shape,dtype=np.complex128)
+        #Evanescent waves
+        ievan = np.where((self.U*self.k)**2>self.N**2)
+        #Propagating waves (excluding where U*k=0, for which m is set to zero) 
+        iprop = np.where(~(((-self.U*self.k)==0) | ((self.U*self.k)**2>self.N**2)))
+    
+        m[ievan] = 1j*np.abs(self.k[ievan])*np.sqrt(1-self.N**2/(self.U*self.k[ievan])**2)
+        # w_g = -Omega*m/kappa**2, so choose sign(m)=-sign(Omega).
+        # for stationary waves, omega=Omega+U*k=0, so Omega=-U*k
+        m[iprop] = -np.sign(-self.U*self.k[iprop])*np.abs(self.k[iprop])*np.sqrt(self.N**2/(self.U*self.k[iprop])**2-1)
+        return m
+        
+class ChannelModel(OneLayerModel):
+    def __init__(self,x,h,U,N,H):
+        super().__init__(x,h,U,N)
+
+        assert(H>0), 'H must be positive'
+        self.H = H
+    
     def solve(self,varname,z):
         """Solve model at specified heights
 
@@ -49,6 +73,63 @@ class LinearModel(object):
         assert(varname in ['eta','u','w','p'])
 
         if np.isscalar(z): z = np.array([z])
+        assert(all((z>=0) & (z<=self.H))), 'All z must lie between 0 and H = {:f}'.format(self.H)
+
+        if varname in ['eta', 'w']:
+            # Solving d**2 eta/dz**2 + m**2 z = 0
+            # Solution of the form eta = A exp(jmz) + B exp(-jmz)
+
+            # For k=0, m=0 which results in division by zero.
+            # Ignore division by zero and set mean mode to zero later on
+            with np.errstate(divide='ignore',invalid='ignore'):
+                eta1 = self.hc[:,np.newaxis] \
+                        / (1 - np.exp(1j*2*self.m[:,np.newaxis]*self.H)) \
+                        * np.exp(1j*self.m[:,np.newaxis]*z)
+                eta2 = -self.hc[:,np.newaxis] \
+                        / (1 - np.exp(1j*2*self.m[:,np.newaxis]*self.H)) \
+                        * np.exp(1j*self.m[:,np.newaxis]*(2*self.H-z))            
+            var = eta1 + eta2
+
+
+            # From definition w = U * d(eta)/dx
+            if varname == 'w': var *= self.U * 1j * self.k[:, np.newaxis]
+        elif varname in ['u', 'p']:
+            # From continuity equation du/dx + dw/dz = 0
+
+            # For k=0, m=0 which results in division by zero.
+            # Ignore division by zero and set mean mode to zero later on
+            with np.errstate(divide='ignore',invalid='ignore'):
+                u1 = -1j * self.m[:, np.newaxis] * self.U * self.hc[:,np.newaxis] \
+                        / (1 - np.exp(1j*2*self.m[:,np.newaxis]*self.H)) \
+                        * np.exp(1j*self.m[:,np.newaxis]*z)
+                u2 = -1j * self.m[:, np.newaxis] * self.U * self.hc[:,np.newaxis] \
+                        / (1 - np.exp(1j*2*self.m[:,np.newaxis]*self.H)) \
+                        * np.exp(1j*self.m[:,np.newaxis]*(2*self.H-z))
+            var = u1 + u2
+
+            # From x-momentum equation U * du/dx = - dp/dx
+            if varname == 'p': var *= -self.U
+
+        # Set mean and defunct modes to zero
+        var[-1,:] = 0.
+        var[0,:]  = 0.
+        return np.squeeze(np.fft.irfft(var,axis=0))
+
+class HalfPlaneModel(OneLayerModel):
+    def solve(self,varname,z):
+        """Solve model at specified heights
+
+        Args:
+            varname (str): name of the variable to be calculated (eta, u, w, or p)
+            z (float or array): heights at which the variable is calculated
+
+        Returns:
+            array: model solution at x coordinates and specified heights
+        """
+        assert(varname in ['eta','u','w','p'])
+
+        if np.isscalar(z): z = np.array([z])
+        assert(all(z>=0)), 'All z must be positive'
 
         if varname == 'eta':
             # Solving d**2 eta/dz**2 + m**2 z = 0
@@ -69,21 +150,4 @@ class LinearModel(object):
         var[-1,:] = 0.
         return np.squeeze(np.fft.irfft(var,axis=0))
 
-    def vertical_wavenumbers(self):
-        """Calculate vertical wavenumbers
-
-        Returns:
-            array: vertical wave numbers
-        """
-        m = np.zeros(self.k.shape,dtype=np.complex128)
-        #Evanescent waves
-        ievan = np.where((self.U*self.k)**2>self.N**2)
-        #Propagating waves (excluding where U*k=0, for which m is set to zero) 
-        iprop = np.where(~(((-self.U*self.k)==0) | ((self.U*self.k)**2>self.N**2)))
-    
-        m[ievan] = 1j*np.abs(self.k[ievan])*np.sqrt(1-self.N**2/(self.U*self.k[ievan])**2)
-        # w_g = -Omega*m/kappa**2, so choose sign(m)=-sign(Omega).
-        # for stationary waves, omega=Omega+U*k=0, so Omega=-U*k
-        m[iprop] = -np.sign(-self.U*self.k[iprop])*np.abs(self.k[iprop])*np.sqrt(self.N**2/(self.U*self.k[iprop])**2-1)
-        return m
 
